320d22715d07e5b126b375b6810f936d85d9e61d
DIUPAKLONG <126437661+DIUPAKLONG@users.noreply.github.com>
2023-03-06 11:47:36 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,90 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # . # # #
+        # . . # .
+        # # # # .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # . # # #
+        # . . # .
+        # # # # .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        # . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # # # # #
+        # # # . .
+        # . . # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # . # # #
+        # . . # .
+        # # # # .
+        `)
+})
+basic.forever(function () {
+    if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.White) && mbit_Robot.Line_Sensor(mbit_Robot.enPos.RightState, mbit_Robot.enLineState.White)) {
+        mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, 80)
+    } else if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.White) && mbit_Robot.Line_Sensor(mbit_Robot.enPos.RightState, mbit_Robot.enLineState.Black)) {
+        mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_SpinRight, 100)
+    } else if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.Black) && mbit_Robot.Line_Sensor(mbit_Robot.enPos.RightState, mbit_Robot.enLineState.White)) {
+        mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_SpinLeft, 100)
+    } else {
+        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Stop)
+    }
+})
